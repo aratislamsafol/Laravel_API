8eaebc9834a60b821a44cdc26b0ba9ebda7f948e
arat <aratislamsafol@gmail.com>
2022-03-13 10:56:02 +0600

    Fresh laravel install

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users=[
+            ['name'=>'arat','email'=>'arat@gmail.com','password'=>'12345678'],
+            ['name'=>'khan','email'=>'khan@gmail.com','password'=>'12345678'],
+            ['name'=>'arats','email'=>'arats@gmail.com','password'=>'12345678'],
+            ['name'=>'arat khan','email'=>'aratkhan@gmail.com','password'=>'12345678'],
+        ]
+    }
+}
